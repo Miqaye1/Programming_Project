0c73311a715fedd2d28246029b8b0ef078f761b5
t3rj831 <terjyannarek@gmail.com>
2026-06-27 01:37:48 +0200

    Reorganized project structure and updated data cleaning code

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load dataset
-df = pd.read_csv("data/raw/cs-training.csv")
+df = pd.read_csv("data/raw/Topic2_credit_assignment_dataset.csv")
 
 # First 5 rows
 print("\n========== FIRST 5 ROWS ==========")
@@ -96,6 +96,73 @@ print(df[df["age"] <= 0])
 for col in df.select_dtypes(include="number"):
     print(col)
     print(df[col].describe())
+
+df.to_csv(
+    "data/cleaned/cs-training-cleaned.csv",
+    index=False
+)
+print("\n========== IMPOSSIBLE VALUES ==========")
+
+print("Age <= 0:", len(df[df["age"] <= 0]))
+print("MonthlyIncome < 0:", len(df[df["MonthlyIncome"] < 0]))
+print("DebtRatio < 0:", len(df[df["DebtRatio"] < 0]))
+print("NumberOfDependents < 0:", len(df[df["NumberOfDependents"] < 0]))
+print("RevolvingUtilizationOfUnsecuredLines < 0:",
+      len(df[df["RevolvingUtilizationOfUnsecuredLines"] < 0]))
+
+print("Rows before:", len(df))
+
+df = df[df["age"] > 0]
+
+print("Rows after:", len(df))
+
+print(df["age"].describe())
+
+print("\nOldest people:")
+print(df.nlargest(10, "age")[["age"]])
+
+print(df.columns.tolist())
+
+print("\n========== OUTLIER COUNT ==========")
+
+for col in df.select_dtypes(include="number"):
+    Q1 = df[col].quantile(0.25)
+    Q3 = df[col].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+
+    outliers = ((df[col] < lower) | (df[col] > upper)).sum()
+
+    print(f"{col}: {outliers}")
+
+df.drop(columns=["Unnamed: 0"], inplace=True)
+print(df.columns)
+
+cols = [
+    "NumberOfTime30-59DaysPastDueNotWorse",
+    "NumberOfTime60-89DaysPastDueNotWorse",
+    "NumberOfTimes90DaysLate"
+]
+
+for col in cols:
+    print("\n", col)
+    print(df[col].value_counts().head(20))
+
+print(df["age"].describe())
+
+print(df.nlargest(10, "age")[["age"]])
+
+cols = [
+    "NumberOfTime30-59DaysPastDueNotWorse",
+    "NumberOfTime60-89DaysPastDueNotWorse",
+    "NumberOfTimes90DaysLate"
+]
+
+for col in cols:
+    print("\n", col)
+    print(df[col].value_counts().head(20))
 
 df.to_csv(
     "data/cleaned/cs-training-cleaned.csv",
